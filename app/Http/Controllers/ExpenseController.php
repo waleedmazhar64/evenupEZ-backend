5ec48126882aa0bed41dec00d8e4c9a3a99c989c
waleedmazhar64 <waleedmazhar64@gmail.com>
2025-02-19 23:06:46 +0000

    receipts endpoint

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Receipt;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class ExpenseController extends Controller
 {
