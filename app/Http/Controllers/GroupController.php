3b36622701ab9241ccd69feb68a7d50ac7fa1e85
waleedmazhar64 <waleedmazhar64@gmail.com>
2025-03-10 12:17:39 +0000

    expenses status

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use DB;
 
 class GroupController extends Controller
 {
